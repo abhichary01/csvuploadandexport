9eaeb0a9c399bceca27bb2494a363fda2e465a27
Abhishek Chary <abhishekchary01@gmail.com>
2023-01-29 19:22:36 +0530

    dd

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const BASEURL = 'http://localhost:3000';
 
 export const environment = {
-  BASE_URL: 'https://raftserver.onrender.com',
-  //BASE_URL: 'http://localhost:3000'
+  //BASE_URL: 'https://raftserver.onrender.com',
+  BASE_URL: 'http://localhost:3000'
 };
